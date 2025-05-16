04909f02e9f95ae3941229e2e572819608157c82
Phroogo <chojecki07@gmail.com>
2025-05-16 22:12:53 +0200

    yes

--- src/net/adam/TriviaGame.java
+++ src/net/adam/TriviaGame.java
@@ -5,34 +5,31 @@ import java.util.Scanner;
 public class TriviaGame {
 
     public static void main(String[] args) {
-        int score = 0;
 
-        String[] que = new String[3];
+        String[] que = new String[5];
         que[0] = "How many States does the USA have?";
         que[1] = "What is the Capital of the United Kingdom";
         que[2] = "What is the chemical symbol for Iron";
-        que[3] = "How High is Mount Everest? (Answer in km)";
+        que[3] = "How High is Mount Everest? (Answer in meters)";
         que[4] = "What is the Answer to Life, the Universe and Everything?";
 
-        String[] ans = new String[3];
+        String[] ans = new String[5];
         ans[0] = "50";
         ans[1] = "london";
         ans[2] = "fe";
         ans[3] = "8848";
         ans[4] = "42";
 
-        triviaGame(que, ans, score);
-
-        while(wantsToContinue()){
-            triviaGame(que, ans, score);
-            score = 0;
-        }
+        do {
+            triviaGame(que, ans);
+        } while (wantsToContinue());
 
     }
 
-    public static void triviaGame(String[] questions, String[] answers, int score) {
+    public static void triviaGame(String[] questions, String[] answers) {
         System.out.println("Welcome to the trivia game!");
         System.out.println("");
+        int score = 0;
 
         for(int i = 0; i < questions.length; i++){
             int t = i + 1;
@@ -40,12 +37,13 @@ public class TriviaGame {
             Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
             input = input.toLowerCase();
+
             if(i == questions.length - 1 && input.equals(answers[i])) {
                 score = scoreIncrease(score);
-                System.out.println("That is correct! Your final score is " + score + "/" + t + " points. If you'd like to play again type 'continue'");
+                System.out.println("That is correct! Your final score is " + score + "/" + t + " points. If you'd like to play again type 'again'");
                 break;
             } else if(i == questions.length - 1 && !input.equals(answers[i])){
-                System.out.println("That is incorrect, the answer was " + answers[i] + ". Your final score is " + score + "/" + t + " point/s. If you'd like to continue type 'continue'");
+                System.out.println("That is incorrect, the answer was " + answers[i] + ". Your final score is " + score + "/" + t + " point/s. If you'd like to continue type 'again'");
                 break;
             }
 
@@ -59,14 +57,16 @@ public class TriviaGame {
             }
         }
     }
+
     public static boolean wantsToContinue() {
         boolean wantsToContinue;
         Scanner scanner = new Scanner(System.in);
         String input = scanner.next();
         input = input.toLowerCase();
-        wantsToContinue = input.equals("continue");
+        wantsToContinue = input.equals("again");
         return wantsToContinue;
     }
+
     public static int scoreIncrease(int score) {
         return score + 1;
     }
