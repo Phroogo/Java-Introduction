e4497fcac3f01f63d799e1147c9f1d38a8790661
Phroogo <chojecki07@gmail.com>
2025-05-16 21:51:19 +0200

    yes

---
+++ src/net/adam/TriviaGame.java
@@ -0,0 +1,70 @@
+package net.adam;
+
+import java.util.Scanner;
+
+public class TriviaGame {
+
+    public static void main(String[] args) {
+        int score = 0;
+
+        String[] que = new String[3];
+        que[0] = "How many States does the USA have?";
+        que[1] = "What is the Capital of the United Kingdom";
+        que[2] = "What is the chemical symbol for Iron";
+
+        String[] ans = new String[3];
+        ans[0] = "50";
+        ans[1] = "london";
+        ans[2] = "fe";
+
+        triviaGame(que, ans, score);
+
+        while(wantsToContinue()){
+            triviaGame(que, ans, score);
+            score = 0;
+        }
+
+    }
+
+    public static void triviaGame(String[] questions, String[] answers, int score) {
+        System.out.println("Welcome to the trivia game!");
+        System.out.println("");
+
+        for(int i = 0; i < questions.length; i++){
+            score = 0;
+            int t = i + 1;
+            System.out.println(questions[i]);
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine();
+            input = input.toLowerCase();
+            if(i == questions.length - 1 && input.equals(answers[i])) {
+                score++;
+                System.out.println("That is correct! Your final score is " + score + "/" + t + " points. If you'd like to play again type 'continue'");
+                break;
+            } else if(i == questions.length - 1 && !input.equals(answers[i])){
+                System.out.println("That is incorrect, the answer was " + answers[i] + ". Your final score is " + score + "/" + t + " point/s. If you'd like to continue type 'continue'");
+                break;
+            }
+
+            if(input.equals(answers[i])) {
+                scoreIncrease(score);
+                System.out.println("That is correct! Your current score is " +  score + "/" + t + ". On to the next question!");
+                System.out.println("");
+            } else{
+                System.out.println("That is incorrect, the answer was " + answers[i] + ". Your current score is " +  score + "/" + t + ". On to the next question!");
+                System.out.println("");
+            }
+        }
+    }
+    public static boolean wantsToContinue() {
+        boolean wantsToContinue;
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+        input = input.toLowerCase();
+        wantsToContinue = input.equals("continue");
+        return wantsToContinue;
+    }
+    public static int scoreIncrease(int score) {
+        return score + 1;
+    }
+}
